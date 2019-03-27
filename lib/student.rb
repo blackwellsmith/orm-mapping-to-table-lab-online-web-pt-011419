d444115f1c7f22825b60fe7db0d8fb31aeb97c70
blackwell <blackwellsmithiv@gmail.com>
2019-03-27 18:52:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,7 @@ class Student
     DROP TABLE students
     SQL
     DB[:conn].execute(sql)
+  end
  end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
